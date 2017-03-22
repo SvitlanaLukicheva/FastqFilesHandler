90d51ceca8dae49f82e70bf318aa2b23e9268354
Svitlana Lukicheva <svitlana.lukicheva@gmail.com>
2017-03-22 22:52:07 +0100

    Integrated usage of CommandLineArguments

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
+
+#include "command_line_arguments.hpp"
+#include "fastq_to_fasta_converter.hpp"
 #include "reads_merger.hpp"
+
 using namespace std;
 
 
@@ -8,44 +12,33 @@ int main(int argc, char* argv[])
     int result = 0;
 
     cout << "=== Welcome to reads merger!\n";
-
-    if(argc % 2 != 0 || argc == 1)
+    CommandLineArguments* arguments = new CommandLineArguments();
+    FastqToFastaConverter* converter;
+    
+    if(!arguments->Parse(argc, argv))
     {
-        cout << "=== FAILURE: Wrong number of parameters specified: " << argc << "\n";
         result = -1;
     }
     else
     {
-        ofstream output (argv[1]);
-        if(output.is_open())
+        if(arguments->JobType == 0)   // reads merger
+        {
+            ReadsMerger* merger = new ReadsMerger(arguments->InputFiles, arguments->OutputFile);
+            result = merger->MergeReads();
+            delete merger;
+        }
+        else if(arguments->JobType == 1)  // fastq to fasta converter
         {
             
-            cout << "=== Output file: " << argv[1] << "\n";
-
-            ReadsMerger* merger = new ReadsMerger();
-            for(int i=2; i < argc - 1; i+=2)
-            {
-                cout << "=== Reading files " << argv[i] << " and " << argv[i+1] << "\n";
-                ifstream input_1(argv[i]);
-                ifstream input_2(argv[i+1]);
-                if(input_1.is_open() && input_2.is_open())
-                    merger->MergeReads(&input_1, &input_2, &output);
-                else
-                {
-                    cout << "=== FAILURE: Unable to open input files\n";
-                    result = -1;
-                    break;
-                }
-            }
-            output.close();
-            delete merger;
         }
         else
         {
-            cout << "=== FAILURE: Unable to open the output file " << argv[1] << "\n";
+            cout << "=== FAILURE: Unknown job type (" << arguments->JobType << "\n";
             result = -1;
         }
     }
+    
+    delete arguments;
 
     cout << "=== Done\n";
     return result;
